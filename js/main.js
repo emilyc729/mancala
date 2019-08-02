3ed8ad0a8029812ac9a0e775e8fe141ffdd70581
Emily <echeun17@mail.ccsf.edu>
2019-08-02 09:02:44 -0700

    exclude clicking pots

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-/*----- constants -----*/ 
+/*----- constants -----*/
 var COLORS = {
-	'1': ['Player A','blue'], // (player 1)
-	'-1': ['Player B', 'red'] // (player 2)
+    '1': ['Player A', 'blue'], // (player 1)
+    '-1': ['Player B', 'red'] // (player 2)
 };
 
-/*----- app's state (variables) -----*/ 
+/*----- app's state (variables) -----*/
 let board, turn, winner;
 
 
-/*----- cached element references -----*/ 
+/*----- cached element references -----*/
 let msgEl = document.getElementById('msg');
 
-/*----- event listeners -----*/ 
+/*----- event listeners -----*/
 document.querySelector('section.board')
     .addEventListener('click', slotClick);
 document.getElementById('reset')
@@ -29,37 +29,53 @@ function init() {
 }
 
 function render() {
-    board.forEach(function(slotVal, slotIdx) {
-        console.log(slotVal);
+    board.forEach(function (slotVal, slotIdx) {
+        //console.log(slotVal);
         let div = document.getElementById(`slot${slotIdx}`);
-        
-        for(let i = 0; i < slotVal; i++) {
+
+        for (let i = 0; i < slotVal; i++) {
             let marble = document.createElement('div');
             marble.classList.add('marble');
             div.appendChild(marble);
         }
 
-        if(winner === null) {
+        if (winner === null) {
             msgEl.textContent = `${COLORS[turn][0]}'s turn`;
         } else {
             msgEl.textContent = `${COLORS[winner][0]}'s Won!`;
         }
-        
-        
+
+
     });
 }
 
 function slotClick(evt) {
     console.log(evt);
     let slotId = 0;
-    console.log(typeof evt.target);
-    if(evt.target.className === 'marble') {
-        slotId = parseInt(evt.target.parentNode.id.replace('slot',''));
+    //console.log(typeof evt.target);
+    if (evt.target.className === 'marble') {
+        slotId = parseInt(evt.target.parentNode.id.replace('slot', ''));
         console.log(slotId);
     } else {
         slotId = parseInt(evt.target.id.replace('slot', ''));
     }
-    console.log(slotId);
+   // console.log(slotId);
+
+    if (slotId === 6 || slotId === 13) return;
+
+    let numMarbles = board[slotId];
+    let curPos = slotId;
+    let nextPos = curPos + 1;
+    while (numMarbles >= 0) {
+        console.log(numMarbles);
+        board[nextPos] += 1;
+        board[curPos] -= 1;
+        nextPos++;
+        numMarbles--;
+
+    }
+
+    /*
     if((turn === 1 && (slotId >= 0 && slotId < 6))
         || (turn === -1 && (slotId >= 7 && slotId < 13))) {
             let numMarbles = board[slotId];
@@ -73,6 +89,7 @@ function slotClick(evt) {
             }
 
     }
+    */
 
 }
 
